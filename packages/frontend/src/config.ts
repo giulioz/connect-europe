dd8d7ecec7e5b803dce7974449be598e7f3dd2a1
Giulio Zausa <giulio.zausa@smc.it>
2020-05-02 13:33:04 +0200

    Config for development

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,5 +1,16 @@
-export default {
+const prodSettings = {
   baseURL: "https://" + window.location.host,
   apiURL: "",
   wsURL: "wss://" + window.location.host + "/",
 };
+
+const devSettings = {
+  baseURL: "http://localhost:8080",
+  apiURL: "http://localhost:8080",
+  wsURL: "ws://localhost:8080/",
+};
+
+const settings =
+  process.env.NODE_ENV === "production" ? prodSettings : devSettings;
+
+export default settings;
